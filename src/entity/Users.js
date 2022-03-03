403f04ef0649ab532882e1b05fe39844c6fdf52b
Renan <rvconessa@gmail.com>
2022-03-03 01:41:07 -0300

    feat: add symbol, add new user and check user exists

--- src/entity/Users.js
+++ src/entity/Users.js
@@ -1,22 +1,28 @@
 import User from './User.js';
 
 // TODO: Criar um Symbol para a propriedade privada 'kUsers'
+const kData = Symbol('kUsers');
 class Users {
   constructor() {
     // TODO: inicializar a propriedade privada 'kData' como uma estrutura importante vista no curso
+    this[kData] = new Set()
   }
 
   add(userRaw) {
     const user = new User(userRaw);
-    // TODO: inserir valor na estrutura escolhida.
+    this[kData].add(user)
   }
 
   hasUsers() {
-    // TODO: Como saber se tem informação dentro da estrutura escolhida?
-    return false;
+    return this[kData].size > 0;
   }
 
   // TODO: Me parece que o objeto gerado precisa ser iterável 🤔
+  *[Symbol.iterator]() {
+    for (const item of this[kData]) {
+      yield item;
+    }
+  }
 }
 
 export default Users;
