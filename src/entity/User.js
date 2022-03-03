741672b639c52a54485bc56e86c9a7b1a9363ec4
Renan <rvconessa@gmail.com>
2022-03-03 01:40:30 -0300

    feat: add coercion type

--- src/entity/User.js
+++ src/entity/User.js
@@ -3,6 +3,10 @@ class User {
     this.id = id;
   }
   // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  [Symbol.toPrimitive](coercionType) {
+    if(coercionType !== "string") throw new TypeError()
+    return `idUser-${this.id}`;
+  }
 }
 
 export default User;
